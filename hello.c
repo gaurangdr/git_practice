ed8157159832d10974187140b389abea45097036
gaurang <gaurangdr@gmail.com>
2017-11-07 11:17:53 +0530

    author name added to hello.c file

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 /*  line added to delete  
  *  hello.c - The simplest kernel module.
+ *  author - gaurang rathod <gaurangdr@gmail.com>
  */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
