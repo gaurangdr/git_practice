11224b6a95bd72ff637e2fe3cca834225fa2cb21
Gaurang Rathod <gaurang.rathod@softnautics.com>
2017-11-07 17:48:07 +0530

    Author name, module description and GPL license added

--- hello.c
+++ hello.c
@@ -5,9 +5,12 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed by module_init */
 
+#define AUTHOR "Gaurang Rathod <gaurang.rathod@softnautics.com>"
+#define DESC "A sample driver"
+
 static int __init hello_init (void)
 {
-	printk(KERN_INFO "Hello world 1.\n");
+	printk(KERN_INFO "Hello world from Kernel module\n");
 
 	/*
 	 * A non 0 return means init_module failed; module can't be loaded.
@@ -17,8 +20,14 @@ static int __init hello_init (void)
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+	printk(KERN_INFO "Goodbye world from Kernel module.\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESC);
+
+MODULE_SUPPORTED_DEVICE("testdevice");
